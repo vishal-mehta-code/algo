ae895f7e518add2dc1216c5ccb06fdfc35dc315f
vishalmehta <er.vishalmehta83@gmail.com>
2025-07-06 20:05:42 +0530

    initial commit

--- src/main/java/string/SmallestMatchingWindow.java
+++ src/main/java/string/SmallestMatchingWindow.java
@@ -1,66 +1,62 @@
 package string;
 
-public class SmallestMatchingWindow {
+import java.util.HashMap;
+import java.util.Map;
 
-    private static final int MAX_CHARS = 256;
+public class SmallestMatchingWindow {
 
     public static void main(String[] args) {
         String str1 = "tist";
-        String str2 = "this is a test string";
+        String str2 = "tstit";
         findMinMatchingWindow(str1.toCharArray(), str2.toCharArray());
     }
 
     private static void findMinMatchingWindow(char[] str1, char[] str2) {
-        int[] count1 = new int[MAX_CHARS];
-
-        for (char current : str1) {
-            ++count1[current];
+        Map<Character, Integer> map1 = new HashMap<>();
+        for (Character curr : str1) {
+            map1.put(curr, map1.getOrDefault(curr, 0) + 1);
         }
 
-        int start = 0;
-        int min = Integer.MAX_VALUE;
-        int[] count2 = new int[MAX_CHARS];
-        for (int i = 0; i < str2.length; i++) {
-            ++count2[str2[i]];
+        int uq = map1.size();
+        int l = 0;
+        int r = 0;
+        int cuq = 0;
+        int maxR = -1;
+        int maxL = -1;
 
-            if (i >= str1.length - 1) {
+        Map<Character, Integer> map2 = new HashMap<>();
 
-                boolean isFound = isFound(count1, count2);
+        while (r < str2.length) {
+            if (map1.containsKey(str2[r])) {
+                map2.put(str2[r], map2.getOrDefault(str2[r], 0) + 1);
 
-                if (isFound) {
-                    while (start <= i - str1.length) {
-                        --count2[str2[start]];
-
-                        if (isFound(count1, count2)) {
-                            start = start + 1;
-                        } else {
-                            ++count2[str2[start]];
-                            break;
-                        }
-                    }
-
-                    if (i - start < min) {
-                        min = i - start;
-                    }
+                if (map2.get(str2[r]) == map1.get(str2[r])) {
+                    cuq++;
                 }
             }
+
+            while (l < r && cuq == uq) {
+                maxL = l;
+                maxR = r;
+                if (map1.containsKey(str2[l])) {
+                    map2.put(str2[l], map2.get(str2[l]) - 1);
+
+                    if (map2.get(str2[l]) < map1.get(str2[l])) {
+                        cuq--;
+                    }
+                }
+                l++;
+            }
+
+            r++;
         }
+
+        System.out.println("maxL : " + maxL + " maxR : " + maxR);
 
         StringBuilder sb = new StringBuilder();
-        for (int i = start; i <= start + min; i++) {
+        for (int i = maxL; i <= maxR; i++) {
             sb.append(str2[i]);
         }
-
-        System.out.println("min window is : " + sb.toString());
+        System.out.println(sb);
     }
-
-    private static boolean isFound(int[] count1, int[] count2) {
-        for (int i = 0; i < MAX_CHARS; i++) {
-            if (count1[i] > 0 && count2[i] < count1[i]) {
-                return false;
-            }
-        }
-        return true;
-    }
-
 }
